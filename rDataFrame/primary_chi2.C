cea54db96609295a7704398b51feeb47703a51c9
fstocker <francesca_stocker@hotmail.com>
2020-01-24 08:38:07 +0100

    adding EventSelection rDataFrame

---
+++ rDataFrame/primary_chi2.C
@@ -0,0 +1,126 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1.h"
+#include "THStack.h"
+#include "TLegend.h"
+#include "TArrow.h"
+#include "TStyle.h"
+#include "TColor.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "lambda.h"
+#include "eventSelection.h"
+#include <ROOT/RDataFrame.hxx>
+
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+
+//using RDataFrame to cut and analyse PionTtrr
+
+using namespace std;
+using namespace ROOT::VecOps;
+
+//compare how Pandora Shower Tag and CNN Track Id can discriminate ChEx signal from the ChExAbs signal, count the matches to the real ChEx signal from MC truth
+
+
+//***********************
+//Main Function
+
+int primary_chi2(const string path = inputFile){
+
+
+   ROOT::RDataFrame mc_frame(inputTree, path);
+   ROOT::RDataFrame data_frame(inputTree, "../../../pionAnalyzerTree/pionana_5387_1GeV_1_16_20.root");
+
+   Int_t legendColor [] = {kCyan-7, kViolet-5, kRed+3};
+   gStyle->SetPalette(3,legendColor);
+   gStyle->SetOptStat("nemr");
+
+   TFile *output = new TFile ("output_primary_chi2.root", "RECREATE");
+   THStack *stack_primary_Chi2 = new THStack("primary_chi2", "Chi2 of Primary Particles");
+   //Start Filtering!
+   //
+   //******MONTE CARLO*********
+   auto mc_definitions = mc_frame
+      .Define("neutron", pdg_neutron)
+      .Define("nucleus", pdg_nucleus)
+      .Define("kaon", pdg_kaon)
+      .Define("muPlus", pdg_muPlus)
+      .Define("muMinus", pdg_muMinus)
+      .Define("gamma", pdg_gamma)
+      .Define("proton", pdg_proton)
+      .Define("piPlus", pdg_piPlus)
+      .Define("piMinus", pdg_piMinus)
+      .Define("electron", pdg_electron)
+      .Define("good_reco", good_reco, {"quality_reco_view_0_wire_backtrack", "quality_reco_view_1_wire_backtrack", "quality_reco_view_2_wire_backtrack", "quality_reco_view_0_max_segment", "quality_reco_view_1_max_segment", "quality_reco_view_2_max_segment"})
+      .Define("manual_beamCut_mc", manual_beamPos_mc, {"reco_beam_startX", "reco_beam_startY", "reco_beam_startZ", "reco_beam_trackDirX", "reco_beam_trackDirY", "reco_beam_trackDirZ", "true_beam_startDirX", "true_beam_startDirY", "true_beam_startDirZ"})
+      .Define("primary_isBeamType", isBeamType, {"reco_beam_type"})
+      .Define("Chi2", "reco_beam_Chi2_proton/reco_beam_Chi2_ndof");
+
+   //
+   //
+   //******DATA ***************
+   auto data_definitions = data_frame
+       .Define("good_reco", good_reco, {"quality_reco_view_0_wire_backtrack", "quality_reco_view_1_wire_backtrack", "quality_reco_view_2_wire_backtrack", "quality_reco_view_0_max_segment", "quality_reco_view_1_max_segment", "quality_reco_view_2_max_segment"})
+      .Define("manual_beamCut_data", manual_beamPos_data, {"reco_beam_startX", "reco_beam_startY", "reco_beam_startZ", "reco_beam_trackDirX", "reco_beam_trackDirY", "reco_beam_trackDirZ","data_BI_X", "data_BI_Y", "data_BI_dirX", "data_BI_dirY", "data_BI_dirZ", "data_BI_nMomenta", "data_BI_nTracks"})
+      .Define("primary_isBeamType", isBeamType, {"reco_beam_type"})
+      .Define("Chi2", "reco_beam_Chi2_proton/reco_beam_Chi2_ndof");
+
+   //
+   //
+   //********HISTOGRAMS*************
+ 
+
+   auto mc_find_primary = mc_definitions
+      .Filter("manual_beamCut_mc")
+      .Filter("primary_isBeamType");
+
+   auto data_find_primary = data_definitions
+      .Filter("manual_beamCut_data")
+      .Filter("primary_isBeamType");
+
+
+
+   auto h_primary_true_pion = mc_find_primary
+      .Filter("reco_beam_true_byHits_PDG == 211").Filter("Chi2>0").Histo1D({"chi2_pion","Pion",125,0.,250.}, "Chi2");
+
+   auto h_primary_true_proton = mc_find_primary
+      .Filter("reco_beam_true_byHits_PDG == 2212").Filter("Chi2>0").Histo1D({"chi2_proton","Proton", 125,0.,250.}, "Chi2");
+
+   auto h_primary_true_muon = mc_find_primary
+      .Filter("reco_beam_true_byHits_PDG == -13").Filter("Chi2>0").Histo1D({"chi2_muon","Muon", 125,0.,250.}, "Chi2");
+
+   auto h_primary_data = data_find_primary
+      .Filter("Chi2>0").Histo1D({"chi2_data", "Data",125, 0.,250.}, "Chi2");
+   h_primary_data->Scale((h_primary_true_pion->Integral() + h_primary_true_proton->Integral() + h_primary_true_muon->Integral())/h_primary_data->Integral());
+
+   std::cout << "Entries in Data = " << *data_definitions.Count() << std::endl;
+   std::cout << "Entries in filtered data Beamline and beam type = " << *data_find_primary.Count() << std::endl;
+
+
+   stack_primary_Chi2->Add(h_primary_true_pion.GetPtr());
+   stack_primary_Chi2->Add(h_primary_true_proton.GetPtr());
+   stack_primary_Chi2->Add(h_primary_true_muon.GetPtr());
+
+   auto c1 = new TCanvas("primary_beam_cand_chi2", "",1600,1200);
+   stack_primary_Chi2->Draw("pfc");
+   h_primary_data->Draw("PSAME");
+   c1->BuildLegend();
+
+   h_primary_data->Write();
+   c1->Write();
+   stack_primary_Chi2->Write();
+
+   
+  
+  
+   output->Write();
+   return 0;
+}
+
---
+++ rDataFrame/test_eventSelection_data.C
@@ -0,0 +1,99 @@
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1.h"
+#include "THStack.h"
+#include "TLegend.h"
+#include "TArrow.h"
+#include "TStyle.h"
+#include "TColor.h"
+#include "TLatex.h"
+#include "TMath.h"
+#include "lambda.h"
+#include <ROOT/RDataFrame.hxx>
+
+
+#include <iostream>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <vector>
+
+//using RDataFrame to cut and analyse PionTtrr
+
+using namespace std;
+using namespace ROOT::VecOps;
+
+//compare how Pandora Shower Tag and CNN Track Id can discriminate ChEx signal from the ChExAbs signal, count the matches to the real ChEx signal from MC truth
+
+
+//***********************
+//Main Function
+
+int test_eventSelection_data(const string path = inputFile){
+
+
+   gInterpreter->GenerateDictionary("vector<vector<int>>", "vector");
+   ROOT::RDataFrame frame(inputTree, path);
+
+   TFile *output = new TFile ("output_test_eventSelection.root", "RECREATE");
+   //Some functions
+
+   auto daugh_proton_like = [](const std::vector<double> &chi2){
+      int all_proton = 1;
+      if(chi2.empty()) return all_proton = -999;
+      for (auto &&vec_chi2 : chi2){
+         if(vec_chi2 > 50.) all_proton = 0;
+      };
+      return all_proton;
+   };
+
+   auto daugh_trk_like = [](const std::vector<double> &trk_score){
+      int all_trk_like = 1;
+      if(trk_score.empty()) return all_trk_like = -999;
+      for (auto &&vec_trk : trk_score){
+         if(vec_trk < 0.4) all_trk_like = 0;
+      };
+
+      return all_trk_like;
+   };
+
+
+   auto filter_truePrimaryPionInel = frame
+      .Define("proton_like_daughters", daugh_proton_like, {"reco_daughter_allTrack_Chi2_proton"})
+      .Define("track_like_daughters", daugh_trk_like, {"reco_daughter_PFP_trackScore"})
+      .Filter("reco_beam_startX > -40.")
+      .Filter("reco_beam_startX < -15.")
+      .Filter("reco_beam_startY > 415.")
+      .Filter("reco_beam_startY < 435.")
+      .Filter("reco_beam_startZ > 0.")
+      .Filter("reco_beam_startZ < 50.")
+      .Filter("reco_beam_endZ < 225.")
+      .Filter("reco_beam_Chi2_proton > 50.") //should be a pion
+      .Filter("reco_beam_type == 13");
+
+   auto snap_absorption = filter_truePrimaryPionInel
+      .Filter("proton_like_daughters == 1")
+      .Filter("track_like_daughters == 1")
+      .Snapshot("tree", "TEST_filter_beam_data_ABS.root");
+
+   auto snap_chex = filter_truePrimaryPionInel
+      .Filter("track_like_daughters == 0")
+      .Snapshot("tree", "TEST_filter_beam_data_ChEx.root");
+   
+   auto snap_noDaugh = filter_truePrimaryPionInel
+      .Filter("track_like_daughters == -999 && proton_like_daughters == -999")
+      .Snapshot("tree", "TEST_filter_beam_data_NO_daughter.root");
+
+   auto snap_other = filter_truePrimaryPionInel
+      .Filter("proton_like_daughters == 0")
+      .Snapshot("tree", "TEST_filter_beam_data_noProtDaugh.root");
+
+
+
+
+   output->Write();
+   return 0;
+}
+
